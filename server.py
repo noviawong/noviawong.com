84e4f4dc7e00d9b0fbc742c0c59ea46b45adbea7
Novia Wong <noviawong@Novias-MacBook-Air.local>
2017-06-06 15:01:31 -0700

    Initial commit

---
+++ server.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/project')
+def project():
+    return render_template('project.html')
+
+@app.route('/research')
+def research():
+    return render_template('research.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/classprojects')
+def classprojects():
+    return render_template('classprojects.html')
+
+@app.route('/independent')
+def independent():
+    return render_template('independent.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
